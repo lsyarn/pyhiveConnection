64e9d647900313102dd76c452ba1a37b38bcfb41
杨李思 <15928928342@189.cn>
2019-11-19 17:20:55 +0800

    format

--- pyhiveConnection/hiveConnector.py
+++ pyhiveConnection/hiveConnector.py
@@ -2,41 +2,45 @@ from kazoo.client import KazooClient
 from pyhive import hive
 import random
 
-#for LUDP Connection
+
+# for LUDP Connection
 def LUDPConnect(username, passwd, database):
     # for LUDP test environment
-    zkhost="node81.it.leap.com:2181,node82.it.leap.com:2181"
-    znodeName="/ludp_hive_ha"
-    serviceKeyword="serverUri"
+    zkhost = "node81.it.leap.com:2181,node82.it.leap.com:2181"
+    znodeName = "/ludp_hive_ha"
+    serviceKeyword = "serverUri"
     return connection(zkhost, znodeName, serviceKeyword, username, passwd, database)
 
-#connect hive by pyhive and return cursor
+
+# connect hive by pyhive and return cursor
 def connection(zkhost, znodeName, serviceKeyword, username, passwd, database):
     hostList = discoveryThriftSerivcehost(zkhost, znodeName, serviceKeyword)
     hostLength = hostList.__len__()
     random.seed()
-    isConnected=False
+    isConnected = False
     while isConnected is False and hostLength > 0:
-        index = random.randint(0, hostLength-1)
+        index = random.randint(0, hostLength - 1)
         hostStr = hostList.pop(index).split(":")
         try:
-            cursor = hive.connect(host=hostStr[0], port=hostStr[1], username=username, password=passwd, auth='LDAP', database=database).cursor()
+            cursor = hive.connect(host=hostStr[0], port=hostStr[1], username=username, password=passwd, auth='LDAP',
+                                  database=database).cursor()
             isConnected = True
         except:
             isConnected = False
             if hostLength > 1:
-                print("ERROR:Can not connect "+hostStr[0]+":"+hostStr[1]+" .try another thrift server...")
+                print("ERROR:Can not connect " + hostStr[0] + ":" + hostStr[1] + " .try another thrift server...")
             else:
                 print("ERROR:Can not connect hiveserver2, please check the connection config and the hiveserver")
                 return 0
         hostLength -= 1
     return cursor
 
-#discovery the thrfit service host list
-def discoveryThriftSerivcehost(zkhost,znodeName,serviceKeyword):
+
+# discovery the thrfit service host list
+def discoveryThriftSerivcehost(zkhost, znodeName, serviceKeyword):
     zkClient = KazooClient(hosts=zkhost)
     zkClient.start()
-    #get the children name of zonde
+    # get the children name of zonde
     result = zkClient.get_children(znodeName)
     zkClient.stop()
     length = result.__len__()
