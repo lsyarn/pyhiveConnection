fc3df71b09522cffa263cd84ed759d9dadc66458
杨李思 <15928928342@189.cn>
2019-11-21 12:53:49 +0800

    add comment

--- pyhiveConnection/hiveConnector.py
+++ pyhiveConnection/hiveConnector.py
@@ -6,18 +6,16 @@ import logging
 logger = logging.getLogger(__name__)
 
 
-# for LUDP Connection
-def ludp_connect(username, passwd, database):
-    # for LUDP test environment
-    zk_host = "node81.it.leap.com:2181,node82.it.leap.com:2181"
-    znode_name = "/ludp_hive_ha"
-    service_keyword = "serverUri"
-    return connection(zk_host, znode_name, service_keyword, username=username, passwd=passwd, database=database)
-
-
-# connect hive by pyhive and return cursor
-def connection(zk_host, znode_name, service_keyword, **kwargs):
-    host_list = discovery_thrift_service_host(zk_host, znode_name, service_keyword)
+def connection(zk_host, znode_name='/hiveserver2', service_keyword='serverUri', **kwargs):
+    """
+    connect hive by pyhive and return cursor
+    :param zk_host: zookeeper host:port, delimited by `,`
+    :param znode_name: hiveserver2 node_name in zookeeper, default `/hiveserver2`
+    :param service_keyword: keyword for hiveserver2 server uri, default `serverUri`
+    :param kwargs: kwargs passed to pyhive.hive.connect
+    :return:
+    """
+    host_list = _discovery_thrift_service_host(zk_host, znode_name, service_keyword)
     host_length = len(host_list)
     random.seed()
     is_connected = False
@@ -29,9 +27,10 @@ def connection(zk_host, znode_name, service_keyword, **kwargs):
             cursor = hive.connect(host=host_str[0], port=host_str[1], **kwargs).cursor()
             is_connected = True
         except Exception as e:
+            logger.debug(e)
             is_connected = False
             if host_length > 1:
-                logger.warning("ERROR:Can not connect " + host_str[0] + ":" + host_str[1] + " .try another thrift server...")
+                logger.warning("ERROR:Can not connect %s:%s.try another thrift server...", host_str[0], host_str[1])
             else:
                 logger.error("ERROR:Can not connect hiveserver2, please check the connection config and the hiveserver")
                 return 0
@@ -41,13 +40,19 @@ def connection(zk_host, znode_name, service_keyword, **kwargs):
     return cursor
 
 
-# discovery the thrfit service host list
-def discovery_thrift_service_host(zk_host, znode_name, service_keyword):
+def _discovery_thrift_service_host(zk_host, znode_name='/hiveserver2', service_keyword='serverUri'):
+    """
+    discovery the thrfit service host list
+    :param zk_host: zookeeper host:port, delimited by `,`
+    :param znode_name: hiveserver2 node_name in zookeeper, default `/hiveserver2`
+    :param service_keyword: keyword for hiveserver2 server uri, default `serverUri`
+    :return: host:port list for hiveserver2
+    """
     zk_client = KazooClient(hosts=zk_host)
     zk_client.start()
     # get the children name of zonde
     result = zk_client.get_children(znode_name)
-    # ['serverUri=salve91:10000;version=1.2.1000.2.6.3.0-235;sequence=0000000458']
+    # result is something like ['serverUri=salve91:10000;version=1.2.1000.2.6.3.0-235;sequence=0000000458']
     zk_client.stop()
     host_list = []
     for server in result:
